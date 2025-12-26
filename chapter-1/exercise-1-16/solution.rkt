9fb950c9b4fe543fab1f9e34af19b318275f02d4
Dmitry <99353550+ddm14159@users.noreply.github.com>
2025-12-26 21:26:17 +0700

    Update solution for exercise 1.16

--- chapter-1/exercise-1-16/solution.rkt
+++ chapter-1/exercise-1-16/solution.rkt
@@ -5,8 +5,12 @@
 ;;; See: https://sicp.hexlet.io/exercises/1.16
 
 ;;; BEGIN SOLUTION
-;;; Write your solution here
-;;; Example:
-;;; (define (solution x)
-;;;   ...)
+(define (square x) (* x x))
+
+(define (solution b n)
+   (define (iter N B A)
+     (cond ((= 0 N) A)
+           ((even? N) (iter (/ N 2) (square B) A))
+           (else (iter (- N 1) B (* B A)))))
+   (iter n b 1))
 ;;; END SOLUTION
