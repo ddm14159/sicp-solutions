ac39c9eb55facc895d5f80bddd84d6100a1d5be8
Dmitry <99353550+ddm14159@users.noreply.github.com>
2025-12-26 21:26:24 +0700

    Update solution for exercise 1.17

--- chapter-1/exercise-1-17/solution.rkt
+++ chapter-1/exercise-1-17/solution.rkt
@@ -5,8 +5,17 @@
 ;;; See: https://sicp.hexlet.io/exercises/1.17
 
 ;;; BEGIN SOLUTION
-;;; Write your solution here
-;;; Example:
-;;; (define (solution x)
-;;;   ...)
+(define (double n)
+    (+ n n))
+
+(define (halve n)
+    (/ n 2))
+
+(define (fast-mul a b)
+    (cond ((= b 0)
+            0)
+          ((even? b)
+            (double (fast-mul a (halve b))))
+          ((odd? b)
+            (+ a (fast-mul a (- b 1))))))
 ;;; END SOLUTION
