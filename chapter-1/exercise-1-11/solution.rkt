ffcd23ae50c7a7870c712e5913f31a318d61ad43
Dmitry <99353550+ddm14159@users.noreply.github.com>
2026-01-16 17:10:28 +0700

    Update solution for exercise 1.11

--- chapter-1/exercise-1-11/solution.rkt
+++ chapter-1/exercise-1-11/solution.rkt
@@ -5,8 +5,20 @@
 ;;; See: https://sicp.hexlet.io/exercises/1.11
 
 ;;; BEGIN SOLUTION
-;;; Write your solution here
-;;; Example:
-;;; (define (solution x)
-;;;   ...)
+#| BEGIN (Введите свое решение) |#
+(define (f x)
+  (if (< x 3)
+      x
+      (+ (f (- x 1)) (* 2 (f (- x 2))) (* 3 (f (- x 3))))))
+
+(define (f-iter x)
+  (define (iter-step a b c count)
+    (if (= count 0)
+        c
+        (iter-step (+ a (* 2 b) (* 3 c)) a b (- count 1))))
+  (if (< x 3)
+      x
+      (iter-step 2 1 0 x)))
+
+#| END |#
 ;;; END SOLUTION
